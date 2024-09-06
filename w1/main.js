860bb158591789a14a3dac798197bb466d6dc730
Christina Cantu <Lovelycantu@gmail.com>
2024-09-05 18:59:28 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+console.log("hello from inside the main.js file")
+
+// let myVar;
+
+const myVar = "Christina";
+const myVarType = typeof(myVar);
+
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+function runNow () {
+    if(myVarType=== "number") {
+        console.log(`will 1 run?`);
+        } else{
+            console.log(`will 13 run?`);
+        }
+}
+
+runNow();
+runNow();
