5b18c1023fba3c20df2781ea57511565fe6bf4f4
Christina Cantu <Lovelycantu@gmail.com>
2024-09-10 11:42:15 -0700

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,33 +1,29 @@
-function determineHouselodPts (){
-    console.log("Inside The Function")
-}
-
+function determinePoints(numberInHousehold) {
+    console.log("Inside the function");
 if (numberInHousehold === 1) {
+
     carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if (numberInHousehold === 2) {
+} else if(numberInHousehold === 2) {
     carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if (numberInHousehold === 3) {
+}  else if(numberInHousehold === 3) {
     carbonFootprintPoints = carbonFootprintPoints + 10;
-} else if (numberInHousehold === 4) {
+} else if(numberInHousehold === 4) {
     carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if (numberInHousehold === 5) {
+} else if(numberInHousehold === 5) {
     carbonFootprintPoints = carbonFootprintPoints + 6;
 } else if(numberInHousehold === 6) {
     carbonFootprintPoints = carbonFootprintPoints + 4;
-} else if (numberInHousehold > 6) {
+} else if(numberInHousehold > 6) {
     carbonFootprintPoints = carbonFootprintPoints + 2;
-} else {
-    console.log("no update to points")
 }
-console.log(`Based on the number of member of the household of ${numberInHousehold} , the points would be ${carbonFootprintPoints}`)
-
-
+console.log(`Based on the number of member of the household of ${numberInHousehold} the points will be ${carbonFootprintPoints}`);
+}
 
 let carbonFootprintPoints = 0;
 //const numberInHousehold = 3;
 
 
-//global scope
+// global scope
 
-determineHouseHoldPts(3)
-determineHouseHoldPts(4)
+determinePoints(3)
+determinePoints(4)
