6689d409b8cbbb0df6dd96f4c992edd9cbc38e09
Christina Cantu <Lovelycantu@gmail.com>
2024-09-11 11:13:39 -0700

    w4 attendance

--- w3/main.js
+++ w3/main.js
@@ -1,51 +1,63 @@
+function determineHouseSizePts(size) {
+    if(size === "large"){
+        carbonFpPoints =carbonFpPoints + 10;
+    } else if(size === "medium") {
+        carbonFpPoints =carbonFpPoints + 7;
+    } else if(size === "small") {
+        carbonFpPoints =carbonFpPoints + 4;
+    } else if(size === "apt") {
+        carbonFpPoints =carbonFpPoints + 2;
+    }
+}
+
 function determinePoints(numberInHousehold) {
     console.log("Inside the function");
 if (numberInHousehold === 1) {
 
-    carbonFootprintPoints = carbonFootprintPoints + 14;
+    carbonFpPoints = carbonFpPoints + 14;
 } else if(numberInHousehold === 2) {
-    carbonFootprintPoints = carbonFootprintPoints + 12;
+    carbonFpPoints = carbonFpPoints + 12;
 }  else if(numberInHousehold === 3) {
-    carbonFootprintPoints = carbonFootprintPoints + 10;
+    carbonFpPoints = carbonFpPoints + 10;
 } else if(numberInHousehold === 4) {
-    carbonFootprintPoints = carbonFootprintPoints + 8;
+    carbonFpPoints = carbonFpPoints + 8;
 } else if(numberInHousehold === 5) {
-    carbonFootprintPoints = carbonFootprintPoints + 6;
+    carbonFpPoints = carbonFpPoints + 6;
 } else if(numberInHousehold === 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 4;
+    carbonFpPoints = carbonFpPoints + 4;
 } else if(numberInHousehold > 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 2;
+    carbonFpPoints = carbonFpPoints + 2;
 }
-console.log(`Based on the number of member of the household of ${numberInHousehold} the points will be ${carbonFootprintPoints}`);
+console.log(`Based on the number of member of the household of ${numberInHousehold} the points will be ${carbonFpPoints}`);
 }
 
-let carbonFootprintPoints = 0;
-//const numberInHousehold = 3;
+let carbonFpPoints = 0;
+//const numberInHousehold = 2;
 
 
 // global scope
 
-determinePoints(3)
-determinePoints(2)
+determinePoints(2);
+determineHouseSizePts("medium");
 
 //Consider the size of your home
 
-function determineHouseSizePoints(SizeOfHouse) {
-    console.log("Inside the function");
-if (SizeOfHouse === "Large House") {
-    carbonFpPoint = carbonFpPoint + 10;
-} else if(SizeOfHouse === "medium-sized house") {
-    carbonFpPoint = carbonFpPoint + 7;
-} else if(SizeOfHouse === "small house") {
-    carbonFpPoint = carbonFpPoint + 4;
-} else if(SizeOfHouse === "apartment") {
-    carbonFpPoint = carbonFpPoint + 2;
-}
-console.log(`Based on the size of your House: ${SizeOfHouse}, the points would be ${carbonFpPoint}`);
-}
+//function determineHouseSizePoints(SizeOfHouse) {
+ //   console.log("Inside the function");
+//if (SizeOfHouse === "Large House") {
+ //   carbonFpPoint = carbonFpPoint + 10;
+//} else if(SizeOfHouse === "medium-sized house") {
+  //  carbonFpPoint = carbonFpPoint + 7;
+//} else if(SizeOfHouse === "small house") {
+  //  carbonFpPoint = carbonFpPoint + 4;
+//} else if(SizeOfHouse === "apartment") {
+  //  carbonFpPoint = carbonFpPoint + 2;
+//}
+//console.log(`Based on the size of your House: ${SizeOfHouse}, the points would be ${carbonFpPoint}`);
+//}
 
-let carbonFpPoint = 0;
+//let carbonFpPoint = 0;
 
 // global scope
 
-determineHouseSizePoints("medium-sized house")
+//determineHouseSizePoints("medium-sized house")
